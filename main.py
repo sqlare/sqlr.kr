f6c0c1140f1396f1d823681319e34dc221be7957
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2023-10-02 18:12:07 +0900

    asd

--- main.py
+++ main.py
@@ -70,12 +70,11 @@ async def shorten_emoji_link(body: Link):
 
     return {"short_link": f"https://sqlr.kr/{key}"}
 
-@app.post("/{short_key}")
-async def redirect_to_original(short_key: str, body: Union[Password, None] = None):
+@app.get("/{short_key}")
+async def redirect_to_original(short_key: str, password: str):
     db_c = redis.Redis(connection_pool=pool())
     db = await db_c.json().jsonget(short_key, Path.root_path())
     await db_c.close()
-    print(db)
     url = bytes.fromhex(db["url"]).decode("utf-8")
     url = base64.b85decode(url).decode("utf-8")
 
@@ -83,7 +82,7 @@ async def redirect_to_original(short_key: str, body: Union[Password, None] = Non
         salt = bytes.fromhex(db["salt"])
         password_hash = bytes.fromhex(db["password_hash"])
 
-        if security(body.password, salt, password_hash).is_correct_password():
+        if security(password, salt, password_hash).is_correct_password():
             return RedirectResponse(url)
 
     except:
