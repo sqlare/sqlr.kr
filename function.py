950e72c3c45f73f5951020e2e837626c8089ebd0
imnyang <nyang.nyanglab@gmail.com>
2023-10-19 13:14:48 +0900

    코체 멍청이

--- function.py
+++ function.py
@@ -1,27 +1,33 @@
+import sqlite3
 from typing import *
-import random
-import string
-import hashlib
-import hmac
-import redis.asyncio as redis
-import secrets
-import emoji
-import metadata_parser
+import random, string, secrets, hashlib, hmac, emoji, metadata_parser
 
-emoji_data = emoji.EMOJI_DATA
-emoji_data = emoji_data.items()
-emoji_list = list()
-for _ in emoji_data:
-    emoji_list.append(_[0])
+class Database:
+    def __init__(self, db_file: str):
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
 
-def get_metadata(url: str):
-    metadata = metadata_parser.MetadataParser(url, search_head_only=False, requests_timeout=10)
-    return metadata.metadata
+        # Create a table if it doesn't exist
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS keys (
+                key TEXT PRIMARY KEY,
+                value TEXT
+            )
+        ''')
+        self.conn.commit()
 
-def pool():
-    return redis.ConnectionPool(host='localhost', port=6379, db=0)
+    def insert_key(self, key: str, value: str):
+        self.cursor.execute("INSERT INTO keys (key, value) VALUES (?, ?)", (key, value))
+        self.conn.commit()
 
-class security():
+    def check_key_exists(self, key: str) -> bool:
+        self.cursor.execute("SELECT value FROM keys WHERE key = ?", (key,))
+        return self.cursor.fetchone() is not None
+
+    def close(self):
+        self.conn.close()
+
+class Security:
     def __init__(self, password: str, salt: bytes = None, password_hash: bytes = None, algorithm: str = 'sha3_256', iterations: int = 100000, dklen: Union[int, None] = None) -> None:
         self.password = password.encode()
         self.salt = salt
@@ -38,32 +44,33 @@ class security():
     def is_correct_password(self) -> bool:
         return hmac.compare_digest(self.password_hash, hashlib.pbkdf2_hmac(self.algorithm, self.password, self.salt, self.iterations, self.dklen))
 
-async def generate_key(length: int = 4) -> AsyncGenerator:
-    __length__ = length
+emoji_data = emoji.EMOJI_DATA
+emoji_data = emoji_data.items()
+emoji_list = list()
+for _ in emoji_data: emoji_list.append(_[0])
+
+def get_metadata(url: str):
+    metadata = metadata_parser.MetadataParser(url, search_head_only=False, requests_timeout=10)
+    return metadata.metadata
+
+async def generate_key(url: str, length: int = 4) -> AsyncGenerator:
+    db = Database('eng.db')  # Database for English keys
     while True:
-        key = ''.join(random.choice(string.ascii_letters) for _ in range(__length__))
+        key = ''.join(random.choice(string.ascii_letters) for _ in range(length))
 
-        db = redis.Redis(connection_pool=pool())
-        db_key = await db.json().get(key)
-        await db.close()
-
-        if db_key == None:
-            yield key
-            break
+        if not db.check_key_exists(key):
+            yield key, url
+            db.insert_key(key, url)  # URL을 값으로 설정
         else:
-            __length__ += 1
+            length += 1
 
-async def generate_emoji_key(length: int = 4) -> AsyncGenerator:
-    __length__ = length
+async def generate_emoji_key(url: str, length: int = 4) -> AsyncGenerator:
+    db = Database('emoji.db')  # Database for Emoji keys
     while True:
         key = ''.join(random.choice(emoji_list) for _ in range(length))
 
-        db = redis.Redis(connection_pool=pool())
-        db_key = await db.json().get(key)
-        await db.close()
-
-        if db_key == None:
-            yield key
-            break
+        if not db.check_key_exists(key):
+            yield key, url
+            db.insert_key(key, url)  # URL을 값으로 설정
         else:
-            __length__ += 1
+            length += 1
--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from fastapi.responses import *
 from fastapi.templating import *
 from fastapi.middleware.cors import *
 from pydantic import *
+from base64 import *
 from typing import *
-from function import *
-from schema import *
-from redis.commands.json.path import Path
-import base64
+import emoji
+import metadata_parser
+import sqlite3
+from sqlite3 import *
+from function import Database, Security, get_metadata, generate_key, generate_emoji_key
 
-app = FastAPI(title="sqlr.kr", description="sqlr.kr 은 링크단축 서비스 입니다.", version="a2.1.0")
+app = FastAPI(
+    title="sqlr.kr",
+    description="sqlr.kr is a URL shortening service.",
+    version="a2.1.0",
+)
 
 origins = [
     "http://sqlr.kr:3000",
@@ -30,70 +36,78 @@ app.add_middleware(
 # Jinja2 템플릿 설정
 templates = Jinja2Templates(directory="templates")
 
+class Link(BaseModel):
+    url: str
+
+# Create the 'keys' table if it doesn't exist
+conn = sqlite3.connect('link.db')
+c = conn.cursor()
+c.execute('''
+    CREATE TABLE IF NOT EXISTS keys (
+        key TEXT PRIMARY KEY,
+        value TEXT
+    )
+''')
+conn.commit()
+conn.close()
+
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/shorten", response_class=ORJSONResponse)
 async def shorten_link(body: Link):
-    key = await anext(generate_key())
+    async for key, url in generate_key(body.url):
+        conn = sqlite3.connect('link.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO keys (key, value) VALUES (?, ?)", (key, url))
+        conn.commit()
+        conn.close()
 
-    url_hash = base64.b85encode(body.url.encode())
-
-    if body.password == None:
-        hgQs = {"url": url_hash.hex()}
-    else:
-        salt, password_hash = security(body.password).hash_new_password()
-        hgQs = {"url": url_hash.hex(), "salt": salt.hex(), "password_hash": password_hash.hex()}
-
-    db = redis.Redis(connection_pool=pool())
-    await db.json().set(key, Path.root_path(), hgQs)
-    await db.close()
-
-    return {"short_link": f"https://sqlr.kr/{key}"}
+        return {"short_link": f"https://sqlr.kr/{key}"}
 
 @app.post("/shorten_emoji", response_class=ORJSONResponse)
 async def shorten_emoji_link(body: Link):
-    key = await anext(generate_emoji_key())
-    
-    url_hash = base64.b85encode(body.url.encode())
-
-    if body.password == None:
-        hgQs = {"url": url_hash.hex()}
-    else:
-        salt, password_hash = security(body.password).hash_new_password()
-        hgQs = {"url": url_hash.hex(), "salt": salt.hex(), "password_hash": password_hash.hex()}
-
-    db = redis.Redis(connection_pool=pool())
-    await db.json().set(key, Path.root_path(), hgQs)
-    await db.close()
+    key = next(generate_emoji_key(body.url))
+    conn = connect('link.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO keys (key, value) VALUES (?, ?)", (key, body.url))
+    conn.commit()
+    conn.close()
 
     return {"short_link": f"https://sqlr.kr/{key}"}
 
 @app.get("/{short_key}")
-async def redirect_to_original(short_key: str, password: Union[str, None] = None):
-    db_c = redis.Redis(connection_pool=pool())
-    db = await db_c.json().jsonget(short_key, Path.root_path())
-    await db_c.close()
-    url = bytes.fromhex(db["url"]).decode("utf-8")
-    url = base64.b85decode(url).decode("utf-8")
+async def redirect_to_original(short_key: str):
+    conn = connect('link.db')
+    c = conn.cursor()
+    c.execute("SELECT value FROM keys WHERE key = ?", (short_key,))
+    result = c.fetchone()
+    conn.close()
 
-    try:
-        salt = bytes.fromhex(db["salt"])
-        password_hash = bytes.fromhex(db["password_hash"])
+    if result:
+        url = result[0]
+        return RedirectResponse(url)
 
-        if security(password, salt, password_hash).is_correct_password():
-            return RedirectResponse(url)
+def get_metadata_from_original(url: str):
+    metadata = get_metadata(url)
+    return metadata
 
-    except:
-        raise HTTPException(status_code=401, detail="Password required or incorrect")
+@app.get("/api/metadata/{short_key}", response_class=ORJSONResponse)
+async def metadata_from_short_link(short_key: str):
+    conn = connect('link.db')
+    c = conn.cursor()
+    c.execute("SELECT value FROM keys WHERE key = ?", (short_key,))
+    result = c.fetchone()
+    conn.close()
 
-@app.get("/api/meta", response_class=ORJSONResponse)
-async def metadata(url: str):
-    return get_metadata(url)
+    if result:
+        url = result[0]
+        metadata = get_metadata_from_original(url)
+        return metadata
+    else:
+        raise HTTPException(status_code=404, detail="Shortened link not found")
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=1111)
-
-# 코체 바보
