eb48f27b14fd9f29678e6aa8a1656ddb10d674d5
imnyang <nyang.nyanglab@gmail.com>
2023-10-22 16:53:18 +0900

    test

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ import metadata_parser
 import sqlite3
 from sqlite3 import *
 from function import get_metadata, generate_key, generate_emoji_key, generate_donate
+import qrcode
+from PIL import Image
+import io
+import base64
 
 app = FastAPI(
     title="sqlr.kr",
@@ -89,8 +93,11 @@ async def shorten_link(body: Link):
 
         return {"short_link": f"https://sqlr.kr/{key}"}
 
-@app.post("/shorten_donate", response_class=ORJSONResponse)
+@app.post("/tossDonate", response_class=ORJSONResponse)
 async def shorten_donate(body: Link):
+    if not body.url.startswith("https://toss.me"):
+        return "이 기능은 무조건 'https://toss.me'로 시작해야해요."
+
     async for key, url in generate_donate(body.url):
         conn = sqlite3.connect('donate.db')
         c = conn.cursor()
@@ -159,7 +166,41 @@ async def metadata_from_short_link(short_key: str):
         metadata = get_metadata_from_original(url)
         return metadata
     else:
-        raise HTTPException(status_code=404, detail="Shortened link not found")
+        return templates.TemplateResponse("404.html", {"request": request}, status_code=404)
+
+@app.get("/generate_qr_code/{short_key}", response_class=HTMLResponse)
+async def generate_qr_code(short_key: str):
+    conn = connect('link.db')
+    c = conn.cursor()
+    c.execute("SELECT value FROM keys WHERE key = ?", (short_key,))
+    result = c.fetchone()
+    conn.close()
+
+    if result:
+        url = result[0]
+        
+        # QR 코드 생성
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(url)
+        qr.make(fit=True)
+        
+        img = qr.make_image(fill_color="black", back_color="white")
+        
+        # 이미지를 바이트 스트림으로 변환
+        img_byte_array = io.BytesIO()
+        img.save(img_byte_array, format="PNG")
+        img_byte_array.seek(0)
+        
+        return HTMLResponse(content=f'<img src="data:image/png;base64,{base64.b64encode(img_byte_array.read()).decode()}" />')
+
+    else:
+        # 404 에러 페이지 표시 (context에 "request" 키 포함)
+        return templates.TemplateResponse("404.html", {"request": request}, status_code=404)
 
 if __name__ == "__main__":
     import uvicorn
